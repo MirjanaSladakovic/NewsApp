6947ba0b3021a94973688a8ce41d03f4d7d332bb
MirjanaSladakovic <mirjanasukalo94@gmail.com>
2022-08-23 20:42:49 +0200

    View implementation
    Implemented Article, Error and News view

--- News/Model/NewsResponse.swift
+++ News/Model/NewsResponse.swift
@@ -17,13 +17,13 @@ struct NewsResponse: Codable {
 // MARK: - Article
 struct Article: Codable, Identifiable {
     let id = UUID()
-    let source: Source
+    let source: Source?
     let author: String?
-    let title: String
+    let title: String?
     let articleDescription: String?
-    let url: String
+    let url: String?
     let urlToImage: String?
-    let publishedAt: Date
+    let publishedAt: Date?
     let content: String?
 
     enum CodingKeys: String, CodingKey {
@@ -37,4 +37,18 @@ struct Article: Codable, Identifiable {
 struct Source: Codable {
     let id: String?
     let name: String
+}
+
+extension Article {
+    
+    static var dummyData: Article {
+        .init(source: Source(id: "the-washington-post", name: "The Washington Post"),
+              author: "Adela Suliman",
+              title: "Stunning Jupiter images shown by NASA's James Webb telescope - The Washington Post",
+              articleDescription: "NASA described the images from the James Webb telescope, showing Jupiter's storms, auroras and faint rings, as « giant news from a giant planet. »",
+              url: "https://www.washingtonpost.com/technology/2022/08/23/jupiter-photo-nasa-webb-telescope/",
+              urlToImage: "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/7L75FQRCFQI63JZPDZYUSBZPXQ.jpg&w=1440",
+              publishedAt: Date(),
+              content: "Comment on this story\r\nStunning images taken by NASAs James Webb Space Telescope show Jupiter in new glory.\r\nFifth in line from the Sun, Jupiter is the largest planet in our solar system more than tw… [+4700 chars]")
+    }
 }
--- News/View/ArticleView.swift
+++ News/View/ArticleView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 import URLImage
 
 struct ArticleView: View {
+    
+    let article: Article
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            if let imgURL = article.urlToImage,
+               let url = URL(string: imgURL) {
+                
+                URLImage(url,
+                         failure: { error, _ in
+                    PlaceholderImageView()
+                },
+                         content: { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                })
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+                
+            } else {
+                PlaceholderImageView()
+            }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                
+                Text(article.title ?? "")
+                    .foregroundColor(.black)
+                    .font(.system(size: 18, weight: .semibold))
+                Text(article.source?.name ?? "")
+                    .foregroundColor(.gray)
+                    .font(.footnote)
+            }
+            
+        }
+    }
+}
+
+struct PlaceholderImageView: View {
+    
+    var body: some View {
+        Image(systemName: "photo.fill")
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .frame(width: 100, height: 100)
     }
 }
 
 struct ArticleView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleView()
+        ArticleView(article: Article.dummyData)
+            .previewLayout(.sizeThatFits)
     }
 }
